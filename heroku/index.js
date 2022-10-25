0b7f203ad9605f4b9f255c3c380f5754f517ca9f
yaroslav <a12011991>
2022-10-25 11:48:22 +0300

    add files

--- heroku/index.js
+++ heroku/index.js
@@ -21,8 +21,7 @@ var token = process.env.TOKEN || 'token';
 var received_updates = [];
 
 app.get('/', function(req, res) {
-  console.log(req);
-  res.send('<pre>' + JSON.stringify(received_updates, null, 2) + '</pre>');
+  res.send("It's working");
 });
 
 app.get(['/facebook', '/instagram'], function(req, res) {
